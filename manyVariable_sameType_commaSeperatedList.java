cd2b1341c0531558dbfdb1aa8f5888599e0ba1e1
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-14 22:33:03 +0530

    11.manyVariable_sameType_commaSeperatedList

---
+++ manyVariable_sameType_commaSeperatedList.java
@@ -0,0 +1,10 @@
+package javaPracticeDaily_w3School;
+
+public class manyVariable_sameType_commaSeperatedList {
+    public static void main(String[] args) {
+        String movie1 =  "HarryPotter ", movie2 = "Titanic ", movie3 = "Avatar ";
+        int num1 = 37, num2 = 33;
+        System.out.println(movie1 + movie2 + movie3);
+        System.out.println(num1 + num2);
+    }
+}
