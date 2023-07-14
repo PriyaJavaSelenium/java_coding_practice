821f2038268e1cdb796459146ca0735215cc2a31
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-14 23:00:56 +0530

    12.diff_data_types

---
+++ diff_data_types.java
@@ -0,0 +1,21 @@
+package javaPracticeDaily_w3School;
+
+public class diff_data_types {
+    public static void main(String[] args) {
+        int rollnum = 35;
+        String name = "Priya";
+        float weight = 63.500f;
+        char blood_group = 'b';
+        double kg = 63.500;
+        Boolean Priya_working_in_IT = true;
+        short year = 1989;
+        long phone_num = 8072559485l;
+        System.out.println(name);
+        System.out.println(weight);
+        System.out.println(blood_group);
+        System.out.println(kg);
+        System.out.println(Priya_working_in_IT);
+        System.out.println(phone_num);
+        System.out.println(year);
+    }
+}
