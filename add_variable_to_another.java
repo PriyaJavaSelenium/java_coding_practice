4680a22bd2c9572f7477213790bdb352b2504918
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-14 22:09:22 +0530

    10.add_variable_to_another

---
+++ add_variable_to_another.java
@@ -0,0 +1,14 @@
+package javaPracticeDaily_w3School;
+
+public class add_variable_to_another {
+    public static void main(String[] args) {
+        String name = "Raksha ";
+        String name1 = "Achu";
+        String siblings = name + name1; // you are supposed to do this
+        System.out.println(siblings);
+
+        System.out.println(name + " and " + name1 + " are siblings"); // instead u did this
+        //try to read question and do properly
+
+    }
+}
