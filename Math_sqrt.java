8b684812f525e26f87d9b25ba7de4f163f518ab5
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 19:30:32 +0530

    23.Math_sqrt

---
+++ Math_sqrt.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class Math_sqrt {
+    public static void main(String[] args) {
+        int a = 64;
+        System.out.println(Math.sqrt(a)); // we can assign a variable seperately
+        System.out.println(Math.sqrt(64)); // we can directly apply number to get sqrt
+    }
+}
