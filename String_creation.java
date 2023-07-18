8c1d4770d8849bba7911cedc6f0f2ebb5250e094
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 12:38:00 +0530

    17.String_creation
    creating a string
    quotes inside a string

---
+++ String_creation.java
@@ -0,0 +1,8 @@
+package javaPracticeDaily_w3School;
+
+public class String_creation {
+    public static void main(String[] args) {
+        String myWork = "Priya is working in IT!!! 'Priya did it!!!'"; // quotes inside a string
+        System.out.println(myWork);
+    }
+}
