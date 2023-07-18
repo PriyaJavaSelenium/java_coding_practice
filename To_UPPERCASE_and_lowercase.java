2b973fb89621a8fbc9bfe66e50c00aa49b656b9b
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 12:47:55 +0530

    19.To_UPPERCASE_and_lowercase
    converting strings to upper case and lower case

---
+++ To_UPPERCASE_and_lowercase.java
@@ -0,0 +1,11 @@
+package javaPracticeDaily_w3School;
+
+import java.sql.SQLOutput;
+
+public class To_UPPERCASE_and_lowercase {
+    public static void main(String[] args) {
+        String myWork = "Priya is working in IT!!! 'Priya did it!!!'"; // quotes inside a string
+        System.out.println(myWork.toUpperCase());
+        System.out.println(myWork.toLowerCase());
+    }
+}
