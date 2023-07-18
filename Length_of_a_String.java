9ed96aa8e8f87af7ab731aa9178d3cf01a090b7b
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 12:42:20 +0530

    18.Length_of_a_String
    finding length of a string

---
+++ Length_of_a_String.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class Length_of_a_String {
+    public static void main(String[] args) {
+        String abtPriya = "Priya is working in IT!!! Priya did it!!!!";
+        System.out.println(abtPriya);
+        System.out.println("Length of above String is = " + abtPriya.length()); // finding length of a string
+    }
+}
