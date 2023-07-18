60b3a4bbe8b50c9e2a89a2d08b0f7cfef688f7b0
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 16:16:25 +0530

    22.Finding_A_String_in_a_string

---
+++ Finding_A_String_in_a_string.java
@@ -0,0 +1,8 @@
+package javaPracticeDaily_w3School;
+
+public class Finding_A_String_in_a_string {
+    public static void main(String[] args) {
+        String a = "Priya is in 'IT'"; // p=0, in=9, is=6
+        System.out.println(a.indexOf("in"));
+    }
+}
