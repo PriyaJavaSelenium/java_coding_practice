88fff2b5874f1339f0848734a003a0af3e6617eb
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 19:40:51 +0530

    23.Math_abs
    when we give negative number it gives positive 
    when we give negative float number it gives positive float
     when we give positive int it gives positive int

---
+++ Math_abs.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class Math_abs {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-8)); // when we give negative number it gives positive
+        System.out.println(Math.abs(-4.7)); // when we give negative float number it gives positive float
+        System.out.println(Math.abs(5)); // when we give positive int it gives positive int
+    }
+}
