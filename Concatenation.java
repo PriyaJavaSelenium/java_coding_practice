ca1ad0c6a47ba63c287500fdda3b126470476041
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 15:26:49 +0530

    20. Concatenation
    adding two strings

---
+++ Concatenation.java
@@ -0,0 +1,8 @@
+package javaPracticeDaily_w3School;
+public class Concatenation {
+    public static void main(String[] args) {
+        String FirstName = "Priya";
+        String SecondName = "AR";
+        System.out.println(FirstName + " " + SecondName);
+    }
+}
