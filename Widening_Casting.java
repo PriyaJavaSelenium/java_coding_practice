8bb50466f126c98f84ceb57f71aad2f341550822
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 11:42:55 +0530

    13. Widening_Casting
    changing int into double
    type casting

---
+++ Widening_Casting.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class Widening_Casting {
+    public static void main(String[] args) {
+        int myNum = 9;
+        double myDouble = myNum;
+        System.out.println(myDouble);
+    }
+}
