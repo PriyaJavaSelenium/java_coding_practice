b769b25c35f1e43cd7b0eaf80511e741fa84a9af
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 15:35:20 +0530

    21.StringNum_Concatenation
    Adding a number and a string, the result will be a string concatenation
    int a = 10;
    String b = "20";
    sout(a+b);
    
    OUTPUT
    1020

---
+++ StringNum_Concatenation.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class StringNum_Concatenation {
+    public static void main(String[] args) {
+        int a = 10; // if you add a number
+        String b = "20"; // and a string
+        System.out.println(a+b); // the result will be a string concatenation
+    }
+}
