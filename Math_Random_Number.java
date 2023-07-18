bc5d3d3540286f821379ab3024489fefbd03092a
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 00:29:51 +0530

    25.Math_Random_Number
    Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive)

---
+++ Math_Random_Number.java
@@ -0,0 +1,7 @@
+package javaPracticeDaily_w3School;
+
+public class Math_Random_Number {
+    public static void main(String[] args) {
+        System.out.println(Math.random()); // Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive)
+    }
+}
