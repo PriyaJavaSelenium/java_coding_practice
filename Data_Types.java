4c9c7e21eea80ff262639c5cb67ad30899013f93
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 12:02:19 +0530

    15. Data_Types
    includes all data types
    byte - 100
    short - 5000
    int - 100000
    long - 15000000000L
    float - 5.75f
    double - 19.99d
    boolean - true false
    char - b
    String - "Hello World"

---
+++ Data_Types.java
@@ -0,0 +1,35 @@
+package javaPracticeDaily_w3School;
+
+//          byte - 100
+//          short - 5000
+//          int - 100000
+//          long - 15000000000L
+//          float - 5.75f
+//          double - 19.99d
+//          boolean - true false
+//          char - b
+//          String - "Hello World"
+public class Data_Types {
+    public static void main(String[] args) {
+        byte myByte = 100;
+        Short myShort = 5000;
+        int myInt = 100000;
+        long myLong = 15000000000L;
+        float myFloat = 5.75f;
+        Double myDouble = 19.99d;
+        boolean Is_Priya_Working_In_IT = true;
+        Character myChar = 'A';
+        String myString = "Priya is working in IT!!!!!!!";
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(Is_Priya_Working_In_IT);
+        System.out.println(myChar);
+        System.out.println(myString);
+
+    }
+}
+
