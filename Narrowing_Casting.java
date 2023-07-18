8fd31ba476b04260553c925ace996f79bdde8259
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-18 11:49:22 +0530

    14. Narrowing_Casting
    from double to int
    its same as int to double, except u need to add 
    (int) myDouble

---
+++ Narrowing_Casting.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class Narrowing_Casting {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myNum = (int) myDouble; // You need to practice this more, u wrote int myNum = myDouble;
+        System.out.println(myNum);
+    }
+}
