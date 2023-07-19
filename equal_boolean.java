cf3aa517c3d05e923dee078ec1b10880490ac299
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 12:38:22 +0530

    29.equal_boolean

---
+++ equal_boolean.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class equal_boolean {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 6;
+        System.out.println(a == b);
+    }
+}
