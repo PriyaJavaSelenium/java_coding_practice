7a3f510751eb5ad1d04a7fc650525733986dca68
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 12:45:36 +0530

    30.if_else

---
+++ if_else.java
@@ -0,0 +1,11 @@
+package javaPracticeDaily_w3School;
+
+public class if_else {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        if (a!=b){
+            System.out.println("a is not equal to b");
+        }
+    }
+}
