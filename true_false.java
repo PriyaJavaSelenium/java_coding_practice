1e7ff7aa2b83aca6ee7e4f9cfa3644ccc76df2d8
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 12:06:45 +0530

    26.true_false

---
+++ true_false.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class true_false {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a>b || a<b);
+    }
+}
