96547c6039f97796fecd521f819b9bd9709ef9b7
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 11:55:52 +0530

    26.Boolean_Type

---
+++ Boolean_Type.java
@@ -0,0 +1,10 @@
+package javaPracticeDaily_w3School;
+
+public class Boolean_Type {
+    public static void main(String[] args) {
+        Boolean is_Priya_Working_in_IT = true;
+        boolean is_Priya_giving_up = false;
+        System.out.println(is_Priya_Working_in_IT);
+        System.out.println(is_Priya_giving_up);
+    }
+}
