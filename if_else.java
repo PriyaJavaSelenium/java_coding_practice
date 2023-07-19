ae68d869df7da97aa9cc95d5af00ebaddf89bf01
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-19 12:55:30 +0530

    30.if_else.java

--- if_else.java
+++ if_else.java
@@ -4,8 +4,12 @@ public class if_else {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        if (a!=b){
+        if(a==b){
+            System.out.println("a is equal to b");
+        } else {
             System.out.println("a is not equal to b");
         }
     }
 }
+
+
