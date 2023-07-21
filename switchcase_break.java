ca52c74db0d5fdb46fa676a713e028c43d58753c
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-21 12:19:35 +0530

    32.switchcase_break

---
+++ switchcase_break.java
@@ -0,0 +1,20 @@
+package javaPracticeDaily_w3School;
+
+public class switchcase_break {
+    public static void main(String[] args) {
+        int day = 3;
+        switch(day){
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+            default:
+                System.out.println("Sunday");
+
+        }
+    }
+}
