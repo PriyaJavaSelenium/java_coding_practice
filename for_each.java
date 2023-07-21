1b6e8624a897f006ec872b659afcd8d1f428fd2b
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-21 15:05:00 +0530

    35.for_each

---
+++ for_each.java
@@ -0,0 +1,10 @@
+package javaPracticeDaily_w3School;
+
+public class for_each {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo" , "BMW" , "Ford" , "Mazda"};
+        for(String i : cars){  // i dont understand for-each, wanna redo...
+            System.out.println(i);
+        }
+    }
+}
