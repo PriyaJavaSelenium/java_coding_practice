457799aeead7c2ba046d6fbdecc8da75c98296c2
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-21 14:33:50 +0530

    34.for_loop

---
+++ for_loop.java
@@ -0,0 +1,21 @@
+package javaPracticeDaily_w3School;
+public class for_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5 ; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
