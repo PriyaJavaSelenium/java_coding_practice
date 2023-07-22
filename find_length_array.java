01aba6cc43ea12485dcb6af25f23bf4c8704bc5a
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-22 14:58:36 +0530

    40.find_length_array

---
+++ find_length_array.java
@@ -0,0 +1,10 @@
+package javaPracticeDaily_w3School;
+
+public class find_length_array {
+    public static void main(String[] args) {
+
+        String[] cars = {"volvo" , "bmw", "audi" , "jaguar"};
+        System.out.println(cars.length);
+
+    }
+}
