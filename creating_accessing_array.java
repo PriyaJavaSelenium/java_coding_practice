595070e635902babfaa9651218dab563e88b14df
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-22 13:17:03 +0530

    38.creating_accessing_array

---
+++ creating_accessing_array.java
@@ -0,0 +1,8 @@
+package javaPracticeDaily_w3School;
+
+public class creating_accessing_array {
+    public static void main(String[] args) {
+        String[] cars = {"volvo" , "bmw", "audi" , "jaguar"};
+        System.out.println(cars[1]);
+    }
+}
