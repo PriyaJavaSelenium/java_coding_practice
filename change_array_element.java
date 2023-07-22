63137288851e429e2b901114e59fb51c60e60921
PriyaJavaSelenium <137667069+PriyaJavaSelenium@users.noreply.github.com>
2023-07-22 14:17:41 +0530

    39.change_array_element

---
+++ change_array_element.java
@@ -0,0 +1,9 @@
+package javaPracticeDaily_w3School;
+
+public class change_array_element {
+    public static void main(String[] args) {
+        String [] cars = {"volvo" , "bmw", "audi" , "jaguar"};
+        cars[0] = "ferrari";
+        System.out.println(cars[0]);
+    }
+}
